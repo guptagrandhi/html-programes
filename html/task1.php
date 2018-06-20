634b200e184179c9c008cba48eb89e3f013bac94
gupta <39546074+guptagrandhi@users.noreply.github.com>
2018-06-20 15:47:37 +0530

    Add files via upload

---
+++ html/task1.php
@@ -0,0 +1,23 @@
+<?php
+$a=array();
+$file_name="sample.csv";
+if(($handle=fopen($file_name,"r"))!== FALSE)
+{
+
+while(($data=fgetcsv($handle,0,","))!== FALSE)
+{
+ $a[]=$data[0].','.$data[1].','.$data[2].','.$data[3].','.$data[4].','.$data[5].','.$data[6].','.$data[7].','.
+ $data[8].','.$data[9].','.$data[10].','.$data[11].','.$data[12].','.$data[13].','.$data[14].','.$data[15].','.
+ $data[16].','.$data[17].','.$data[18].','.$data[19].','.$data[20].','.$data[21].','.$data[22].','.$data[23].','.
+ $data[24].','.$data[25].','.$data[26].','.$data[27].','.$data[28].','.$data[29].','.$data[30];
+}
+fclose($handle);
+echo 'success';
+}
+$fp=fopen('chinnu15.csv','w');
+foreach($a as $l)
+{
+	fputcsv($fp, array($l),',','');
+}
+fclose($fp);
+?>
